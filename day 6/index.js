f65365fcb4f60117eddff99ce81f16dc125e3d87
Abhinav872006 <abhinavj9852@gmail.com>
2024-12-02 15:48:20 +0530

    Add files via upload

---
+++ day 6/index.js
@@ -0,0 +1,95 @@
+
+// // // // console.log("---Welcome in jadui duniya---");
+
+// // // // // variables : var,let,const:
+
+// // // // document.write("Hello World"); 
+
+// // // // let a = 5;
+// // // // console.log(a)
+// // // // var b = 5;
+// // // // console.log(b)
+// // // // const c = 10;
+// // // // console.log(c)
+// // // // a = 9;
+// // // // b = 5;
+// // // // // c = 9;
+// // // // a = "abc";
+// // // // console.log(a)
+// // // let i = 1,num=100;
+// // // let count = 0;
+// // // while (i<=num) {
+// // //   if (i%2!==0) {
+// // //     console.log(`${i} is odd number`)
+// // //     count++;
+// // //   }
+// // //   i++;
+// // // }
+// // // console.log(`There are ${count} odd number`) 
+
+// // // for(let i = 0;i <= 100;i++){
+// // // if(i % 3 === 0 && i % 5 === 0); {
+// // //     console.log(i)
+// // // }
+// // // }
+
+
+// // for(let i=0;i<100;i++ ){
+// //     if(i%2===0)
+// //         console.log(i)
+// // }
+
+
+
+
+
+
+// // let i=0;
+// // while(  i<=100){
+// //     if (i%2===0){
+// //     console.log(i);
+// // }
+// //     i++;
+
+    
+// // }
+
+// let i=0;
+// while(  i<=100){
+//     if (i%2 !== 0){
+//     console.log(i);
+// }
+//     i++;
+
+    
+// }
+
+
+let i=0;
+while(i>=1 && i<=100){
+    
+        console.log(i)
+    
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+let ans = 0;
+for (let i= 0 ;i<100;i+2){
+    ans = i +ans;
+}
+console.log(ans);
+
+
+
+
